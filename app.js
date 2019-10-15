d53535c8dc49318318d98f427719f32e10605096
pakaratAriya <pakarat_ariya@hotmail.com>
2019-10-15 16:02:33 -0700

    Update app.js

--- app.js
+++ app.js
@@ -74,6 +74,6 @@ app.delete("/deleteArtist/:artistId",(req,res)=>{
     })
 })
 
-app.listen(process.env.PORT,process.env.IP,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("SERVER HAS STARTED")
 })
